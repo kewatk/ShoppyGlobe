f2e26a826e80e74490f030cdec6c3edc602f4c20
kewatk <kewat7698@gmail.com>
2025-12-29 21:45:44 +0530

    styling Product item make attractive

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,15 +1,17 @@
+
 function ProductItem({ product }) {
   return (
-    <div className="product-item">
+    <div className="product-card">
       <img
         src={product.thumbnail}
         alt={product.title}
         className="product-cover"
       />
-
-      <h1 className="product-title">{product.title}</h1>
-      <h1 className="product-price">{product.price}$</h1>
-      {/* <p className="product-description">{product.description}</p> */}
+      <div className="product-details">
+        <h1 className="product-title">{product.title}</h1>
+        <h1 className="product-price">{product.price}$</h1>
+        <p className="product-description">{product.description}</p>
+      </div>
       <button className="add-cart">Add to Cart</button>
     </div>
   );
